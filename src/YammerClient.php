4a7f4b0ecf8b5e8401b447c4ed36df2af75d354d
suhaboncukcu <suha.boncukcu@portera.nl>
2018-02-28 13:48:59 +0300

    Update comment lines

--- src/YammerClient.php
+++ src/YammerClient.php
@@ -2,46 +2,87 @@
 
 namespace Yammer;
 
+
 /**
- *
+ * Class YammerClient
+ * @package Yammer
  */
 class YammerClient
 {
 
-  const API_URL = 'https://www.yammer.com/api/';
-  const VERSION = 'v1';
-  private $client_id;
-  private $token;
-  private $httpClient;
-  private $response;
-  private $content;
-  private $body;
+    /**
+     *
+     */
+    const API_URL = 'https://www.yammer.com/api/';
+    /**
+     *
+     */
+    const VERSION = 'v1';
+    /**
+     * @var
+     */
+    private $client_id;
+    /**
+     * @var
+     */
+    private $token;
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private $httpClient;
+    /**
+     * @var
+     */
+    private $response;
+    /**
+     * @var
+     */
+    private $content;
+    /**
+     * @var
+     */
+    private $body;
 
-  function __construct()
-  {
-      $this->client_id = config('yammer.client_id');
-      $this->token = config('yammer.token');
-      $this->httpClient = new \GuzzleHttp\Client();
-  }
+    /**
+     * YammerClient constructor.
+     */
+    function __construct()
+    {
+        $this->client_id = config('yammer.client_id');
+        $this->token = config('yammer.token');
+        $this->httpClient = new \GuzzleHttp\Client();
+    }
 
-  public function getMessages($options = [])
-  {
-      $this->body = $this->httpClient->request(
-        'GET',
-        $this->_getFullUrl().'/messages.json?' . http_build_query($options), [
-        'headers' => [
-          'Authorization' => 'Bearer ' . $this->token
-        ]
-      ]);
+    /**
+     * @param array $options
+     *
+     * @return $this
+     */
+    public function getMessages($options = [])
+    {
+        $this->body = $this->httpClient->request(
+            'GET',
+            $this->_getFullUrl().'/messages.json?' . http_build_query($options), [
+            'headers' => [
+              'Authorization' => 'Bearer ' . $this->token
+            ]
+        ]);
 
-      $this->body = $this->_jsonToArray($this->body->getBody());
-      $this->content = $this->getBody('messages');
+        $this->body = $this->_jsonToArray($this->body->getBody());
+        $this->content = $this->getBody('messages');
 
-      return $this;
-  }
+        return $this;
+    }
 
-  public function getMessagesByGroupId($groupId = null, $options = [])
-  {
+    /**
+     * @param null $groupId
+     * @param array $options
+     *
+     * @return $this
+     * @throws \Exception
+     */
+    public function getMessagesByGroupId($groupId = null, $options = [])
+    {
       if (is_null($groupId)) {
         throw new \Exception( __FUNCTION__ . " expects a group ID", 200);
       }
@@ -57,10 +98,15 @@ class YammerClient
       $this->body = $this->_jsonToArray($this->body->getBody());
       $this->content = $this->getBody('messages');
       return $this;
-  }
+    }
 
-  public function getUsers($userId = null)
-  {
+    /**
+     * @param null $userId
+     *
+     * @return mixed
+     */
+    public function getUsers($userId = null)
+    {
       $target = $this->_getFullUrl() . '/users';
 
       $target .= is_null($userId) ? '.json' : '/' . $userId . '.json';
@@ -75,10 +121,16 @@ class YammerClient
 
       $response = $this->_jsonToArray($response->getBody());
       return $response;
-  }
+    }
 
-  public function getBody($key = '', $type = null)
-  {
+    /**
+     * @param string $key
+     * @param null $type
+     *
+     * @return array|mixed|null
+     */
+    public function getBody($key = '', $type = null)
+    {
       $result = $dataset = $this->body[$key];
 
       if (!is_null($type)) {
@@ -86,107 +138,146 @@ class YammerClient
       }
 
       return $result;
-  }
+    }
 
-  public function reverse()
-  {
+    /**
+     * @return $this
+     */
+    public function reverse()
+    {
       $this->content = array_reverse($this->content);
 
       return $this;
-  }
+    }
 
-  public function withUsers()
-  {
-      $users = $this->getBody('references', 'user');
+    /**
+     * @return $this
+     */
+    public function withUsers()
+    {
+        $users = $this->getBody('references', 'user');
 
-      $this->content = array_map(function($message) use ($users) {
+        $this->content = array_map(function($message) use ($users) {
 
-          $message['user'] = $this->_search($message['sender_id'], 'id', $users, 'first');
+            $message['user'] = $this->_search($message['sender_id'], 'id', $users, 'first');
 
-          if ($message['liked_by']['count'] > 0) {
-              foreach ($message['liked_by']['names'] as $likeKey => $name) {
-                  $message['liked_by']['names'][$likeKey]['user'] = $this->_search($name['user_id'], 'id', $users, 'first');
-              }
-          }
-
-          if (count($message['notified_user_ids']) > 0) {
-            foreach ($message['notified_user_ids'] as $notifyKey => $notifiedUserId) {
-                $message['notified_user_ids'][$notifyKey] = $this->_search($notifiedUserId, 'id', $users, 'first');
+            if ($message['liked_by']['count'] > 0) {
+                foreach ($message['liked_by']['names'] as $likeKey => $name) {
+                    $message['liked_by']['names'][$likeKey]['user'] = $this->_search($name['user_id'], 'id', $users, 'first');
+                }
             }
-          }
 
-          return $message;
-      }, $this->content);
+            if (count($message['notified_user_ids']) > 0) {
+                foreach ($message['notified_user_ids'] as $notifyKey => $notifiedUserId) {
+                    $message['notified_user_ids'][$notifyKey] = $this->_search($notifiedUserId, 'id', $users, 'first');
+                }
+            }
 
-      return $this;
-  }
+            return $message;
+        }, $this->content);
 
-  public function where($search_key = '', $search_value = '')
-  {
-      $this->content = array_where($this->content, function($value, $key) use ($search_key, $search_value) {
+        return $this;
+    }
+
+    /**
+     * @param string $search_key
+     * @param string $search_value
+     *
+     * @return $this
+     */
+    public function where($search_key = '', $search_value = '')
+    {
+        $this->content = array_where($this->content, function($value, $key) use ($search_key, $search_value) {
           return $value[$search_key] == $search_value;
-      });
+        });
 
-      return $this;
-  }
+        return $this;
+    }
 
-  public function getStatusCode()
-  {
-      return $this->response->getStatusCode();
-  }
+    /**
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->response->getStatusCode();
+    }
 
-  public function excludeReplies()
-  {
-      $this->content = array_where($this->content, function($value, $key) {
+    /**
+     * @return $this
+     */
+    public function excludeReplies()
+    {
+        $this->content = array_where($this->content, function($value, $key) {
           return $value['replied_to_id'];
-      });
-      return $this;
-  }
+        });
+        return $this;
+    }
 
-  public function onlyReplies()
-  {
-      $this->content = array_where($this->content, function($value, $key) {
+    /**
+     * @return $this
+     */
+    public function onlyReplies()
+    {
+        $this->content = array_where($this->content, function($value, $key) {
           return !$value['replied_to_id'];
-      });
-      return $this;
-  }
+        });
+        return $this;
+    }
 
-  public function get()
-  {
-      return $this->content;
-  }
+    /**
+     * @return mixed
+     */
+    public function get()
+    {
+        return $this->content;
+    }
 
-  private function _getFullUrl()
-  {
-      return self::API_URL.self::VERSION;
-  }
+    /**
+     * @return string
+     */
+    private function _getFullUrl()
+    {
+        return self::API_URL.self::VERSION;
+    }
 
-  private function _jsonToArray($dataset)
-  {
-    return json_decode($dataset, TRUE);
-  }
+    /**
+     * @param $dataset
+     *
+     * @return mixed
+     */
+    private function _jsonToArray($dataset)
+    {
+        return json_decode($dataset, TRUE);
+    }
 
-  private function _search($find = '', $column = '', $dataset = [], $option = '')
-  {
-      $result = [];
+    /**
+     * @param string $find
+     * @param string $column
+     * @param array $dataset
+     * @param string $option
+     *
+     * @return array|mixed|null
+     */
+    private function _search($find = '', $column = '', $dataset = [], $option = '')
+    {
+        $result = [];
 
-      foreach ($dataset as $key => $data) {
-          if ($data[$column] == $find) {
-              array_push($result, $data);
-          }
-      }
+        foreach ($dataset as $key => $data) {
+            if ($data[$column] == $find) {
+                array_push($result, $data);
+            }
+        }
 
-      if(!empty($result)){
-          if ($option == 'first') {
-              return current($result);
-          }else if ($option == 'last') {
-              return end($result);
-          }else {
-              return $result;
-          }
-
-      }else {
-          return null;
-      }
-  }
+        if(!empty($result)){
+            if ($option == 'first') {
+                return current($result);
+            } else if ($option == 'last') {
+                return end($result);
+            } else {
+                return $result;
+            }
+        } else {
+            return null;
+        }
+    }
 }
